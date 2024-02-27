f877e3f92887fe1798f61d85e2214816dfc6cab6
Janoh <you@example.com>
2024-02-27 15:29:14 +0000

    Setting up databricks workflow

---
+++ 0a54b96ac143_dag.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': 'https://dbc-b54c5c54-233d.cloud.databricks.com/?o=1865928197306450#notebook/4292106396610984/command/612106955201492',
+}
+
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+
+default_args = {
+    'owner': 'Janoh',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('0a54b96ac143_dag',
+    # should be a datetime format
+    start_date=datetime(2024, 2, 27),
+    # check out possible intervals, should be a string
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id="1108-162752-8okw8dgg",
+        notebook_task=notebook_task
+    )
+    opr_submit_run
+
